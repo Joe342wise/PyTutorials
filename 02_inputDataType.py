40002b8dc08944dae8fa2ebaa6749ced19ff48a8
Joseph Osei Yaw Nyarko <jnyarko342@gmail.com>
2026-03-20 19:40:42 +0000

    Add string manipulation examples and exercises in 02_inputDataType.py and exercise.py to enhance understanding of string operations and transformations.

--- 02_inputDataType.py
+++ 02_inputDataType.py
@@ -45,3 +45,46 @@ print("Hello" + 18) # This will raise an error because you cannot concatenate a 
 print("Hello" + str(18)) # This will work because we have converted the integer to a string
 print("Hello" + "18") # This will work because we have converted the integer to a string
 print("Hello" + "18") # This will work because we have converted the integer to a string
+
+# Strings Manipulation
+# Strings are immutable, which means that they cannot be changed after they are created.
+#  We can use the "len()" function to get the length of a string.
+#  We can use the "upper()" function to convert a string to uppercase.
+#  We can use the "lower()" function to convert a string to lowercase.
+#  We can use the "capitalize()" function to convert the first letter of a string to uppercase.
+#  We can use the "title()" function to convert the first letter of each word in a string to uppercase.
+#  We can use the "strip()" function to remove whitespace from the beginning and end of a string.
+#  We can use the "split()" function to split a string into a list of substrings.
+
+# For example:
+course = "Python for Beginners"
+# With this variable "course" we can say that we now have a string object contained in the variable "course".
+# We can access the string object by using the variable name "course".
+# We can access the string object by using the index number of the string object.
+# The index number is the position of the character in the string object.
+# The index number starts at 0 and goes to the length of the string object - 1.
+# We can also access the string object by using a range of index numbers.
+# With the string object we can perform all the various functions that was mentioned above.
+
+# For example:
+print(course[0]) # This will print the first character of the string object
+print(course[1]) # This will print the second character of the string object
+print(course[2]) # This will print the third character of the string object
+print(course[3]) # This will print the fourth character of the string object
+print(course[4]) # This will print the fifth character of the string object
+print(course[0:3]) # This will print the first three characters of the string object
+print(course[4:]) # This will print the fifth character of the string object and all the characters after it
+print(course[:5]) # This will print the first five characters of the string object
+print(course[1:-1]) # This will print the second character of the string object and all the characters before the last character
+print(course[1:-1]) # This will print the second character of the string object and all the characters before the last character
+print(course.upper()) # This will print the string object in uppercase
+print(course.lower()) # This will print the string object in lowercase
+print(course.capitalize()) # This will print the first letter of the string object in uppercase
+print(course.title()) # This will print the first letter of each word in the string object in uppercase
+print(course.strip()) # This will remove whitespace from the beginning and end of the string object
+print(course.split()) # This will split the string object into a list of substrings
+print(course.join()) # This will join the string object into a list of substrings
+print(course.replace()) # This will replace the string object with a new string object
+print(course.find()) # This will find the index number of the string object
+print(course.count()) # This will count the number of occurrences of the string object
+print(course.len()) # This will get the length of the string object
--- exercise.py
+++ exercise.py
@@ -38,3 +38,73 @@
 
 # Write the program here:
 
+
+
+# Exercise 3 (String Manipulation):
+# String Analyzer
+# Program Requirements:
+
+# Given the string:
+text = "   python programming is fun   "
+# Write a Python program that performs the following tasks:
+
+# Part 1: Basic Exploration
+# Print the original string
+# Print the length of the string (including spaces)
+
+# Part 2: Cleaning the String
+# Remove the extra spaces at the beginning and end
+# Store the cleaned string in a new variable
+# Print the cleaned string
+
+# Part 3: String Transformations
+
+# Convert the cleaned string to:
+# UPPERCASE
+# lowercase
+# Title Case
+# Capitalized
+
+# Part 4: Indexing & Slicing
+# Print:
+# The first character
+# The last character
+# The first 6 characters
+# The last 3 characters
+# Part 5: Working with Words
+# Split the string into a list of words
+# Print the list
+# Count how many words are in the string
+
+# Part 6: Searching & Replacing
+# Find the position of the word "programming"
+# Replace "fun" with "awesome"
+# Count how many times the letter "o" appears
+
+# Expected Output Example:
+# part 1:
+# Original:    python programming is fun   
+# Length: 34
+
+# part 2:
+# Cleaned: python programming is fun
+
+# part 3:
+# Uppercase: PYTHON PROGRAMMING IS FUN
+# Title Case: Python Programming Is Fun
+# Capitalized: Python Programming Is Fun
+
+# part 4:
+# First character: p
+# Last character: n
+# First 6 characters: python
+# Last 3 characters: fun
+
+# part 5:
+# Words: ['python', 'programming', 'is', 'fun']
+# Number of words: 4
+
+# part 6:
+# Position of 'programming': 7
+# Replaced: python programming is awesome
+# Count of 'o': 2
