4a2bd1c65f44782003e7288be960ec3faa3ec227
Joseph Osei Yaw Nyarko <jnyarko342@gmail.com>
2026-04-25 21:05:34 +0000

    Refactor variable definitions and comments for clarity in 01_variables.py; correct typos and improve formatting.

--- 01_variables.py
+++ 01_variables.py
@@ -11,17 +11,17 @@ Age = 18  # variable = value (value can be a string, int, boolean, float, etc)
 
 print(Age)
 
-age = 20 # intiger
-name = "Yayra" # string
-is_active = True # Boolean
-isActive = False # Boolean
-decimal = 18.5 # Float
+age = 20  # intiger
+name = "Yayra"  # string
+is_active = True  # Boolean
+isActive = False  # Boolean
+decimal = 18.5  # Float
 
 print(age + " is an intiger")
 
 
 # tyoes of casing
-full_name = "Lois" # snake-case
-other-name = "Adams Mawuli" # snake-case
-fullName = "Yayra" # camelCase
-dateOfBirth = "01/01/2007" #camelCase
+full_name = "Lois"  # snake-case
+other_name = "Adams Mawuli"  # snake-case
+fullName = "Yayra"  # camelCase
+dateOfBirth = "01/01/2007"  # camelCase
